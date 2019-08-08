da22ae42c7c6f75b6ca49086b15c426450a5186f
Patrick Kennedy <pkennedytx1@gmail.com>
2019-08-07 20:07:14 -0500

    added the card component, need to solidofy colors and get content in order

---
+++ ui/components/ProjectsTimerCard/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ProjectTimerCardStyle } from './styles';
+
+const ProjectTimerCard = () => (
+  <ProjectTimerCardStyle>
+    <h3>This is to show that the card is all good.</h3>
+  </ProjectTimerCardStyle>
+);
+
+export default ProjectTimerCard;

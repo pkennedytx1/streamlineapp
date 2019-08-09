9e562b01d23c0b7fb14b4b5a0e8b2c3d13e85f70
Patrick Kennedy <pkennedytx1@gmail.com>
2019-08-08 20:53:13 -0500

    added the previousdayadata component to house the pie graph of the time spent the day before on certain projects, the styling of the card is shown, bug on the margin of the container stops it from reaching the true end of the div

---
+++ ui/components/PreviousDayData/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { PreviousDayDataStyle } from './styles';
+
+const PreviousDayData = () => (
+  <PreviousDayDataStyle>
+    <div>This shows the card is functional</div>
+  </PreviousDayDataStyle>
+);
+
+export default PreviousDayData;

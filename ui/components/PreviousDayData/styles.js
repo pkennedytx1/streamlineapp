33cb43d61fb7c10669c4174c58d27a6e0b0bbf2d
Patrick Kennedy <pkennedytx1@gmail.com>
2019-08-09 19:36:32 -0500

    the react-chart-2 was installed and a basic pie chart dislpayed as a component within the card. Need to add 2 more and start getting the front end logic for the buttons for adding projects worked out.

--- ui/components/PreviousDayData/styles.js
+++ ui/components/PreviousDayData/styles.js
@@ -2,5 +2,8 @@ import styled from 'styled-components';
 
 // eslint-disable-next-line import/prefer-default-export
 export const PreviousDayDataStyle = styled.div`
-  background-color: red;
+  border: solid 1px #e6e6e6;
+  border-radius: 2px;
+  padding: 10px;
+  background-color: #f2f2f2;
 `;

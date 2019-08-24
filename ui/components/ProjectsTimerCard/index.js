c9ebebaa08b3a4741f4cff8ca2eefa07067389de
Patrick Kennedy <pkennedytx1@gmail.com>
2019-08-24 15:36:45 -0500

    added the modal to input a new project, need to add the functionality and start the database

--- ui/components/ProjectsTimerCard/index.js
+++ ui/components/ProjectsTimerCard/index.js
@@ -1,5 +1,9 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
+import { Modal } from 'react-bootstrap';
 // eslint-disable-next-line import/named
 import { ProjectTimerCardStyle } from './styles';
 import Timer from '../Timer';
@@ -10,7 +14,18 @@ class ProjectTimerCard extends React.Component {
     super(props);
     this.state = {
       projectTitle: 'Please Choose a Project',
+      show: false,
     };
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
   }
 
   render() {
@@ -25,10 +40,28 @@ class ProjectTimerCard extends React.Component {
             </h3>
           </div>
           <div className="col-md-6">
-            <div>
+            <div style={{ position: 'relative' }}>
               <h4>Current Projects</h4>
+              <a onClick={this.handleShow}>
+                <h6 style={{ position: 'absolute', right: '0', top: '-5px', cursor: 'pointer' }}>
+                  + New Project
+                </h6>
+              </a>
+
+              <Modal show={this.state.show} onHide={this.handleClose}>
+                <button className="btn-danger" type="button" onClick={this.handleClose}>
+                  Close
+                </button>
+                <Modal.Header>
+                  <h3>New Project Form</h3>
+                </Modal.Header>
+                <Modal.Body>
+                  <p>Here is where everything is going.</p>
+                </Modal.Body>
+              </Modal>
+
               <div>
-                <button type="button" className="btn btn-success">
+                <button type="button" className="btn btn-primary btn-block">
                   Project Title
                 </button>
               </div>
